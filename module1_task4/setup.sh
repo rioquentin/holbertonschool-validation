b36d04e27f4efa3c7e56fde18c0f8e1b9aa41136
root <you@example.com>
2023-01-04 03:56:04 -0800

    add echo in setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,7 +1,4 @@
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
 apt-get update && apt-get install -y hugo make
-if [ $? -eq 0 ]; then
-  mkdir dist
-  cp -r build/* dist
 make build
 
