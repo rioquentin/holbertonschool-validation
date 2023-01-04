feca47812640d3f4c44bffac31cab7fe5d106117
root <you@example.com>
2023-01-04 04:17:24 -0800

    update at extented hugo

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get install -y hugo make
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_0.109.0_linux-amd64.deb last.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb last.deb
 apt install last.deb
 rm last.deb
 make build
