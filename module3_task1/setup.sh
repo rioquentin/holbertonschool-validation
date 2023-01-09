5c50999f18868612621653b72a6c5e3570fabb26
root <you@example.com>
2023-01-09 01:46:11 -0800

    add sudo to all etup?sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-apt-get update && apt-get install -y hugo make
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
-apt install ./last.deb
-rm last.deb
+sudo apt-get update && sudo apt-get install -y hugo make
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+sudo apt install ./last.deb
+sudo rm last.deb
 make build
 
